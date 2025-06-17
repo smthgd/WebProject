17f3a6e95b15246402d3f136a5dc1aa63be29d17
smthgd <sem060504@gmail.com>
2025-06-17 10:44:27 +0500

    создана бэкенд структура

---
+++ backend/Controllers/RoomController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace DefaultNamespace;
+
+public class RoomController : ControllerBase
+{
+    
+}
