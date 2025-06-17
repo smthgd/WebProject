25af7862c0be27f456e58ae3d9d229b9a3ff0661
smthgd <sem060504@gmail.com>
2025-06-17 14:30:11 +0500

    реализовал метод получения фильмов из API кинопоиска

--- backend/Models/Movie.cs
+++ backend/Models/Movie.cs
@@ -12,3 +12,29 @@ public class Movie
 
     public ICollection<Room> Rooms { get; set; }
 }
+
+public class KinopoiskResponse
+{
+    public List<KinopoiskMovie> Docs { get; set; }
+}
+
+public class KinopoiskMovie
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Rating Rating { get; set; }
+
+    public Poster Poster { get; set; }
+}
+
+public class Rating
+{
+    public double Kp { get; set; }
+}
+
+public class Poster
+{
+    public string Url { get; set; }
+}
