a88e19eb499c39b6bc1d8acbcb7fb9f30fde61b8
smthgd <sem060504@gmail.com>
2025-06-17 13:16:43 +0500

    реализовал метод создания комнаты

--- backend/Controllers/RoomController.cs
+++ backend/Controllers/RoomController.cs
@@ -1,10 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
+using backend.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace DefaultNamespace;
 
 public class RoomController : ControllerBase
 {
+    private static Dictionary<Room, List<Movie>> rooms = new Dictionary<Room, List<Movie>>();
     
+    private readonly WebProjectDbContext _context;
+
+    public RoomController(WebProjectDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateRoom()
+    {
+        var random = new Random();
+        int roomCode = (int)(DateTime.UtcNow.Ticks % 1000000000) + random.Next(1, 1000);
+
+        var room = new Room 
+        { 
+            CreationDate = DateTime.UtcNow,
+            Id = roomCode,
+        };
+
+        _context.Rooms.Add(room);
+        await _context.SaveChangesAsync();
+
+        rooms[room] = new List<Movie>();
+
+        return Ok(roomCode);
+    }
 }
