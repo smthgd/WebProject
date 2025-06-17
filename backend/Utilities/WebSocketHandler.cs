5b4e736f1112c9174f4d62a285c64bdbbd3d5992
PoSayDone <poseaydone@ya.ru>
2025-06-17 18:24:33 +0500

    feature: добавил хэндлер вебсокета

--- backend/Utilities/WebSocketHandler.cs
+++ backend/Utilities/WebSocketHandler.cs
@@ -7,4 +7,22 @@ public class WebSocketHandler
 {
     private static Dictionary<int, WebSocket> _sockets = new Dictionary<int, WebSocket>();
 
+    public async Task HandleWebSocket(WebSocket webSocket, int userId)
+    {
+        _sockets[userId] = webSocket;
+
+        await SendMessage(userId, $"userId: {userId}");
+
+        while (webSocket.State == WebSocketState.Open)
+        {
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                _sockets.Remove(userId);
+            }
+        }
+    }
 }
