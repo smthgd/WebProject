c9e65b12f944474d533bf603dac56dcb86eb0dfa
PoSayDone <poseaydone@ya.ru>
2025-06-18 23:23:57 +0500

    fix: хардкод адерса api

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,6 @@
-export const API_URL = process.env.VITE_API_URL || "http://localhost:5065";
+export const API_URL =
+	process.env.VITE_API_URL ||
+	"http://choosy-backend-8bmcbw-368642-77-110-105-3.traefik.me";
 
 export const config = {
 	apiUrl: API_URL,
